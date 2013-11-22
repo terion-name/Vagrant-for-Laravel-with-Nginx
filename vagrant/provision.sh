9f8e457e562df3888cc6ee6117e0a56a1acc8c53
Terion <mail@terion.name>
2013-11-22 14:19:10 +0200

    added mongodb

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -23,6 +23,13 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again p
 echo "--- Redis time ---"
 sudo apt-get install -y redis-server
 
+echo "--- MongoDB time ---"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
+sudo /etc/init.d/mongodb start
+
 echo "--- Installing base packages ---"
 sudo apt-get install -y vim curl python-software-properties
 
@@ -34,6 +41,8 @@ sudo apt-get install -y vim curl python-software-properties
 
 echo "--- Installing PHP-specific packages ---"
 sudo apt-get install -y php5-dev php5-fpm php5-cli php-pear nginx php5-curl php5-gd php5-imagick php5-mcrypt mysql-server mysql-client php5-mysql git-core
+sudo pecl install mongo
+sudo sh -c 'echo "extension=mongo.so" >> /etc/php5/fpm/php.ini'
 
 echo "--- Installing and configuring Xdebug ---"
 sudo apt-get install -y php5-xdebug
